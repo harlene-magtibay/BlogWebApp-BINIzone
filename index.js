4517463d14ac192800e0ad232bf31155c10f0635
harlene-magtibay <magtibayharlene08@gmail.com>
2025-06-22 20:19:45 +0800

    made arrays to serve as db

--- index.js
+++ index.js
@@ -20,15 +20,32 @@ app.get("/compose", (req, res) => {
   res.render("compose.ejs");
 })
 
+let musicArray = [];
+let perfArray = [];
+let fashionArray = [];
+let biniplusArray = [];
+
 app.post("/submit", (req, res) => {
-  function blogPost (category, title, content) {
-    this.category = category;
+  function blogPost (title, content) {
     this.title = title;
     this.content = content;
   }
-  const blogPost1 = new blogPost(req.body.category, req.body.title, req.body.content);
-
-  console.log("Post received:", blogPost1);
+  
+  if (req.body.category === "Music") {
+    musicArray.push(new blogPost(req.body.title, req.body.content));
+    console.log("Post received. Updated Music Array:", musicArray);
+    console.log("Latest Post: Title - ", musicArray[musicArray.length - 1].title, "| Content - ", musicArray[musicArray.length - 1].content);
+  } else if (req.body.category === "Performances") {
+    perfArray.push(new blogPost(req.body.title, req.body.content));
+    console.log("Post received. Updated Performances Array:", perfArray);
+  } else if (req.body.category === "Fashion & Concepts") {
+    fashionArray.push(new blogPost(req.body.title, req.body.content));
+    console.log("Post received. Updated Fashion & Concepts Array:", fashionArray);
+  } else {
+    biniplusArray.push(new blogPost(req.body.title, req.body.content));
+    console.log("Post received. Updated BINI+ Array:", biniplusArray);
+  }
+  
   res.render("compose.ejs", { showSuccess: true });
 });
 
