fcd2716a24fd71c86f67f1061c843ada7eb29790
harlene-magtibay <magtibayharlene08@gmail.com>
2025-06-22 13:09:59 +0800

    create post page

--- index.js
+++ index.js
@@ -9,15 +9,29 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: '10mb' }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs");
 })
 
 app.get("/compose", (req, res) => {
   res.render("compose.ejs");
 })
+
+app.post("/submit", (req, res) => {
+  function blogPost (category, title, content) {
+    this.category = category;
+    this.title = title;
+    this.content = content;
+  }
+  const blogPost1 = new blogPost(req.body.category, req.body.title, req.body.content);
+
+  console.log("Post received:", blogPost1);
+  res.render("compose.ejs", { showSuccess: true });
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
